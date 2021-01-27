6a81dcc3025548c5e44a90760f38581068398477
cheems <xenonstaforums@gmail.com>
2021-01-27 18:18:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5598,6 +5598,9 @@ function nav(path) {
             <button class="mdui-textfield-icon mdui-btn mdui-btn-icon" onclick="if($('#search_bar').hasClass('mdui-textfield-expanded') && $('#search_bar_form>input').val()) $('#search_bar_form').submit();">
                 <i class="mdui-icon material-icons">search</i>
             </button>
+            <button class="mdui-textfield-icon mdui-btn mdui-btn-icon" onclick="if($('#search_bar').hasClass('mdui-textfield-expanded') && $('#search_bar_form>input').val()) $('#search_bar_form').submit();">
+                <i class="mdui-icon material-icons">info_outline</i>
+            </button>
             <form id="search_bar_form" method="get" action="/${cur}:search">
             <input class="mdui-textfield-input" type="text" name="q" placeholder="Search in current drive" value="${search_text}"/>
             </form>
