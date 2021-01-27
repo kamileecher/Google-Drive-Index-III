5ad022b16e550852e86b7bde55d1c823fa0c8596
cheems <xenonstaforums@gmail.com>
2021-01-27 16:49:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5511,18 +5511,7 @@ function init() {
   );
   var html = `
 <header class="nexmoe-nav">
-	<div class="navSize">
-			<a href="/"><img class="avatar" src="${UI.avatar}"/></a>
-			<div class="nav_menu"  style="display:${
-        UI.disable_navicon == false ? "none" : "block"
-      }">
-				<ul class="menu_ul">
-					<li class="menu_li"><a href="https://telegram.dog/cheems" target="_blank">Contact</a></li>
-					<li class="menu_li"><a href="https://github.com/cheems/Google-Drive-Index-III" target="_blank">Github</a></li>
-				</ul>
-				<div class="nav_icon" ></div>
-			</div>
-	</div>
+	
 </header>
 <div class="mdui-container">
 	<div class="mdui-container-fluid">
